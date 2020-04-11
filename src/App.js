cd1895e90a116c897a49423b38d9bebbcacc7ec1
Ripul Chhabra <ripulchhabra29@gmail.com>
2020-04-12 00:54:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Docker is super
+         Docker is super hit
         </p>
         <a
           className="App-link"
